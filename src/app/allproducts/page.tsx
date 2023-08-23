8205175a2fe947726d1fe12b8800215a6a12b3c1
Ahmad Bilal <ahmadbilal339@gmail.com>
2023-08-24 00:07:40 +0500

    all product

--- src/app/allproducts/page.tsx
+++ src/app/allproducts/page.tsx
@@ -3,33 +3,15 @@ import React from "react";
 import FetchData from "../../../sanity/FetchData";
 import { urlForImage } from "../../../sanity/lib/image";
 import Link from "next/link";
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
 import Image from "next/image";
 
 
 
 export default async function page() {
   const data = await FetchData();
-  const responsive = {
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 3,
-    },
-    tablet: {
-      breakpoint: { max: 1023, min: 464 },
-      items: 2,
-    },
-    mobile: {
-      breakpoint: { max: 767, min: 0 },
-      items: 1,
-    },
-  };
-  console.log("data", data[0].slug);
 
   return (
-    <div>
-      <Carousel responsive={responsive}>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-8 gap-4 pt-10 md:py-20 ">
         {data.map((product: any) => (
           <Link key={product._id} href={`product/${product.slug.current}`}>
             <Image
@@ -44,7 +26,6 @@ export default async function page() {
             <p className="font-semibold text-lg">{product.price}</p>
           </Link>
         ))}
-      </Carousel>
     </div>
   );
 }
